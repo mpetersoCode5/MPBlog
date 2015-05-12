5ff5ea5f58f8b08aa054e4e3bbf18be36a3f0164
Michael Peterson <michaelpeterson@Michaels-iMac.local>
2015-05-12 19:59:23 -0400

    Continued setting up base blog functionaity: PHP

---
+++ PHP/admin/Add-Post.php
@@ -0,0 +1,86 @@
+<?php
+include('menu.php');
+?>
+
+<form action='' method='post'>
+	
+	<p><label>Title</label><br />
+	<input type='text' name='postTitle' value='<?php if(isset($error){ echo $_POST['postTitle'];}?>'></p>
+	
+	<p><label>Description</label><br />
+	<textarea name='postDesc' cols='60' rows='10'><?php if(isset($error)){ echo $_POST['postDesc'];}?></textarea></p>
+	
+	<p><label>Content</label><br />
+	<textarea name='postCont' cols='60' rows='10'><?php if(isset($error)){ echo $_POST['postCont'];}?></textarea></p>
+	
+	<p><input type='submit' name='submit' value='Submit'></p>
+	
+</form>
+
+<script src="//tinymce.cachefly.net/4.0/tinymce.min.js"></script>
+<script>
+        tinymce.init({
+            selector: "textarea",
+            plugins: [
+                "advlist autolink lists link image charmap print preview anchor",
+                "searchreplace visualblocks code fullscreen",
+                "insertdatetime media table contextmenu paste"
+            ],
+            toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+        });
+</script>
+		
+<?php
+if(isset($_POST['submit']))
+{
+	$_POST = array_map('stripslashes', $_POST);
+	
+	//collect form data
+	extract($_POST);
+	
+	//very basic validation
+	if($postTitle == '')
+	{
+		$error[] = 'Please enter the title.';
+	}
+	
+	if($postDesc == '')
+	{
+		$error[] = 'Please enter the description.';
+	}
+	
+	if($postCont == '')
+	{
+		$error[] = 'Please enter the content';
+	}
+}
+
+if(!isset($error))
+{
+	try
+	{
+		//insert into database
+		$stmt = $db->prepare('INSERT INTO blog_posts(postTitle, postDesc, postCont, postDate) VALUES (:postTitle, :postDesc, :postCont, :postDate)');
+		$stmt->exectue(array(
+			':postTitle' => $postTitle,
+			':postDesc' => $postDesc,
+			':postCont' => $postCont,
+			':postDate' => date('Y-m-d H:i:s')
+		));
+		
+		//redirect to index page
+		header('Location: index.php?action=added');
+		exit;
+	} catch(PDOException $e) {
+		echo $e->getMessage();
+	}
+}
+
+if(isset($error))
+{
+	foreach($error as $error){
+		echo '<p class="error">'.$error.'</p>'
+	}
+}
+
+?>
--- PHP/admin/Login.php
+++ PHP/admin/Login.php
@@ -10,4 +10,30 @@
 ?>	
 	<form action="" method="post">
 		<p><label>Username</label><input type="text" name ="username" value=""/></p>
-		<p><label>Password</label>
+		<p><label>Password</label><input type="password" name="password" value="" /></p>
+		<p><label></label><input type="submit" name="submit" value="Login" /></p>
+	</form>
+	
+<?php
+
+	if(isset($_POST['submit']))
+	{
+		$username = trim($_POST['username']);
+		$password = trim($_POST['password']);
+		
+		if($user->login($username, $password))
+		{
+			header('Location: index.php');
+			exit;
+		} 	else {
+			$message = '<p class="error">Wrong username or password</p>';
+		}
+	}
+	
+	if(isset($message))
+	{
+		echo $message;
+	}
+		
+?> 
+			
